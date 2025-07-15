75b6c29a07086d8294434de5141bcf9da2483729
wanreain <reduan.alias@outlook.com>
2025-07-15 12:18:37 +0800

    Delete NovaTrader-Full directory

--- NovaTrader-Full/dashboard.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from strategy import fetch_data, apply_strategy
-from config import SYMBOL
-
-st.title("📊 NovaTrader Dashboard")
-df = fetch_data(SYMBOL)
-df = apply_strategy(df)
-st.line_chart(df[['close', 'ema_fast', 'ema_slow']])
-st.dataframe(df.tail(10))
